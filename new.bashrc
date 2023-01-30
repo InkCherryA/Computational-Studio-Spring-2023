9ef0214077a70d72fa3f9657033b86d9793bcbe2
rabbitblood <qz154772610@gmail.com>
2023-01-30 15:41:19 -0800

    new file:   new.bashrc made bashrc file to use as initial data for new.sh
    modified:   new.sh
    added bashrc file to new.sh
    changed structure for faster execution

---
+++ new.bashrc
@@ -0,0 +1,1 @@
+currentFileIdx=7
--- new.sh
+++ new.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-x=0
-y=$(printf "%04d" $x)
 
-while [ 1 == 1 ]
-do
-  if [ ! -e p$y ]
-  then
-    cp -r template p$y
-    mv p$y/tmp.css p$y/$y.css
-    mv p$y/tmp.html p$y/$y.html
-    sed -i -e "s/tmp/$y/" p$y/$y.html
-    break
-  fi
-  x=$((x+1))
-  y=$(printf "%04d" $x)
-done
+#intialize bashrc
+source ./new.bashrc
+
+y=$(printf "%04d" $currentFileIdx)
+
+# create file and update bashrc
+cp -r template p$y
+mv p$y/tmp.css p$y/$y.css
+mv p$y/tmp.html p$y/$y.html
+sed -i -e "s/tmp/$y/" p$y/$y.html
+
+#update bashrc
+currentFileIdx=$((currentFileIdx+1))
+sed -i -e "s/currentFileIdx=$(($currentFileIdx-1))/currentFileIdx=$currentFileIdx/" new.bashrc
+
+# display file create information
+y=$(printf "%04d" $currentFileIdx)
 
